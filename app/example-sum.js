323be6026848a7db20c42c334b66dd25f180fd57
Cristiano <pino.xman@gmail.com>
2021-04-19 18:02:51 +0200

    first - draft.

---
+++ app/example-sum.js
@@ -0,0 +1,10 @@
+
+let MyClass01 = require('../module/example/example-module');
+
+let m = new MyClass01("Pippo");
+m.show();
+
+function sum(a, b) {
+    return a + b;
+  }
+module.exports = sum;
---
+++ module/example/example-module.js
@@ -0,0 +1,11 @@
+class MyExample01{
+    constructor(name){
+        this.name = name;
+    }
+
+    show(){
+        console.log("" + this.name);
+    }
+}
+
+module.exports = MyExample01;
