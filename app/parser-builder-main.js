9629488349dd05eda4de369431aca03c8ce20a07
Cristiano <pino.xman@gmail.com>
2021-07-13 15:19:03 +0200

    minor fix

--- app/parser-builder-main.js
+++ app/parser-builder-main.js
@@ -5,16 +5,6 @@ const { getJsonFromSplit, getTemplateHtml, getOutputPath, getCss, DataSource } =
 const parser = require('../lib/parser-json.js');
 const pretty = require('pretty');
 
-
-// const confObj = {
-//     outname: "dati_generali_cabine_cabine_secondarie.html",
-//     files: ["config.json", "dati_generali.json", "cabine_secondarie.json"]
-//     // outname: "dati_generali_scomparti.html",
-//     // files: ["config.json", "dati_generali.json", "scomparti.json"]
-//     // outname: "dati_generali_trasformatori_mt_bt.html",
-//     // files: ["config.json", "dati_generali.json", "trasformatori_mt_bt.json"]
-// };
-
 const confArray = [
     {
         outname: "dati_generali_cabine_cabine_secondarie.html",
