3c8a8d97f6c5a43ab7aa6d96887936db88716836
Cristiano <pino.xman@gmail.com>
2021-08-05 09:12:09 +0200

    Config for 2021.08.02-Digitalizz_SchedaRaccDati_U8

--- app/parser-builder-main.js
+++ app/parser-builder-main.js
@@ -10,23 +10,19 @@ const confArray = [
     {
         outname: "dati_generali.html",
         files: ["config.json", "dati_generali.json"]
-    }
-    ,
+    },
     {
         outname: "cabine_secondarie.html",
         files: ["config.json", "cabine_secondarie.json"]
-    }
-    ,
+    },
     {
         outname: "scomparti.html",
         files: ["config.json", "scomparti.json"]
-    }
-    ,
+    },
     {
         outname: "trasformatori_mt_bt.html",
         files: ["config.json", "trasformatori_mt_bt.json"]
-    }
-    ,
+    },
     // {
     //     outname: "derivazioni_bt.html",
     //     files: ["config.json", "derivazioni_bt.json"]
@@ -35,33 +31,28 @@ const confArray = [
     {
         outname: "quadro_bt.html",
         files: ["config.json", "quadro_bt.json"]
-    }
-    ,
-    {
-        outname: "dati_generali_cabine_secondarie.html",
-        files: ["config.json", "dati_generali.json", "cabine_secondarie.json"]
-    }
-    ,
-    {
-        outname: "dati_generali_scomparti.html",
-        files: ["config.json", "dati_generali.json", "scomparti.json"]
-    }
-    ,
-    {
-        outname: "dati_generali_trasformatori_mt_bt.html",
-        files: ["config.json", "dati_generali.json", "trasformatori_mt_bt.json"]
-    }
-    ,
-    {
-        outname: "dati_generali_derivazioni_bt.html",
-        files: ["config.json", "dati_generali.json", "derivazioni_bt.json"]
-    }
-    ,
-    {
-        outname: "dati_generali_bt_quadro_bt.html",
-        files: ["config.json", "dati_generali.json", "quadro_bt.json"]
-    }
-    // ,
+    },
+    // {
+    //     outname: "dati_generali_cabine_secondarie.html",
+    //     files: ["config.json", "dati_generali.json", "cabine_secondarie.json"]
+    // },
+    // {
+    //     outname: "dati_generali_scomparti.html",
+    //     files: ["config.json", "dati_generali.json", "scomparti.json"]
+    // },
+    // {
+    //     outname: "dati_generali_trasformatori_mt_bt.html",
+    //     files: ["config.json", "dati_generali.json", "trasformatori_mt_bt.json"]
+    // },
+    // {
+    //     outname: "dati_generali_derivazioni_bt.html",
+    //     files: ["config.json", "dati_generali.json", "derivazioni_bt.json"]
+    // },
+    // {
+    //     outname: "dati_generali_bt_quadro_bt.html",
+    //     files: ["config.json", "dati_generali.json", "quadro_bt.json"]
+    // }
+// ,
     // {
     //     outname: "dati_generali_derivazioni_bt_quadro_bt.html",
     //     files: ["config.json", "dati_generali.json", "derivazioni_bt.json", "quadro_bt.json"]
