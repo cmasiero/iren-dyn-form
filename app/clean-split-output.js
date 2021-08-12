45141691ce8654ffe9d39e4b72f65bc8d8413bc2
Cristiano <pino.xman@gmail.com>
2021-08-12 16:18:24 +0200

    cleaner

---
+++ app/clean-split-output.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const directories = [
+    './config/json_split',
+    './output'
+]
+
+directories.forEach( directory => {
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+      
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+                else console.log(path.join(directory, file).concat(' REMOVED!'));
+            });
+        }
+      });
+});
