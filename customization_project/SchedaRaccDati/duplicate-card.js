593abd1c3507054829a807608cc0e5b9be22ad0f
Cristiano <pino.xman@gmail.com>
2021-09-22 16:38:58 +0200

    first commit

---
+++ customization_project/SchedaRaccDati/duplicate-card.js
@@ -0,0 +1,150 @@
+const fs = require('fs');
+const path = require('path');
+
+const makeScomparti = (config) => {
+
+    let resultAll = {};
+    let idxes = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"];
+    idxes.forEach(idx => {
+
+        // let idx = "01";
+        let sourceData = fs.readFileSync(config.source, 'utf8');
+        let outData = sourceData.replace(/"scomparti"/g, `"scomparti_${idx}"`);
+        outData = outData.replace(/"Scomparti"/g, `"Scomparti ${idx}"`);
+        outData = outData.replace(/rule_/g, `rule${idx}_`);
+        outData = outData.replace(/sc_/g, `sc${idx}_`);
+
+        let jsonObject = JSON.parse(outData);
+
+        let idxSc = idx === "01" ? "" : (parseInt(idx) - 1).toString();
+        idxSc = idxSc.length === 1 ? "0".concat(idxSc) : idxSc;
+        let sc = Object.assign({ 'rule': `visible if sc${idxSc}_ulteriore_scomparto_gestire == 'SI'` }, jsonObject[`scomparti_${idx}`]);
+
+        let resultSingle = {};
+        resultSingle[`scomparti_${idx}`] = sc;
+        fs.writeFileSync(config.destination + '/' + `scomparti${idx}.json`, JSON.stringify(resultSingle, null, 2));
+
+        resultAll[`scomparti_${idx}`] = sc;
+
+    });
+
+    fs.writeFileSync(config.destination + '/' + `scompartiAll.json`, JSON.stringify(resultAll, null, 2));
+
+};
+
+const makeTrasformatoriMtBt = (config) => {
+
+    let resultAll = {};
+    let idxes = ["01", "02", "03"];
+    idxes.forEach(idx => {
+
+        // let idx = "01";
+        let sourceData = fs.readFileSync(config.source, 'utf8');
+        let outData = sourceData.replace(/"trasformatori_mt_bt"/g, `"trasformatori_mt_bt_${idx}"`);
+        outData = outData.replace(/"Trasformatori MT-BT"/g, `"Trasformatori MT-BT ${idx}"`);
+        // outData = outData.replace(/rule_/g, `rule${idx}_`);
+        outData = outData.replace(/tr_/g, `tr${idx}_`);
+
+        let jsonObject = JSON.parse(outData);
+
+        let idxSc = idx === "01" ? "" : (parseInt(idx) - 1).toString();
+        idxSc = idxSc.length === 1 ? "0".concat(idxSc) : idxSc;
+                                                       
+        let sc = Object.assign({ 'rule': `visible if tr${idxSc}_ulteriore_trasformatore_gestire == 'SI'` }, jsonObject[`trasformatori_mt_bt_${idx}`]);
+
+        let resultSingle = {};
+        resultSingle[`trasformatori_mt_bt_${idx}`] = sc;
+        fs.writeFileSync(config.destination + '/' + `trasformatori_mt_bt_${idx}.json`, JSON.stringify(resultSingle, null, 2));
+
+        resultAll[`trasformatori_mt_bt_${idx}`] = sc;
+
+    });
+
+    fs.writeFileSync(config.destination + '/' + `trasformatori_mt_btAll.json`, JSON.stringify(resultAll, null, 2));
+
+};
+
+const makeDerivazioniBt = (config) => {
+
+    let resultAll = {};
+    let idxes = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"];
+    idxes.forEach(idx => {
+
+        let sourceData = fs.readFileSync(config.source, 'utf8');
+        let outData = sourceData.replace(/"derivazioni_bt"/g, `"derivazioni_bt_${idx}"`);
+        outData = outData.replace(/"Derivazioni BT"/g, `"Derivazioni BT ${idx}"`);
+        // outData = outData.replace(/rule_/g, `rule${idx}_`);
+        outData = outData.replace(/dbt_/g, `dbt${idx}_`);
+
+        let jsonObject = JSON.parse(outData);
+
+        let idxSc = idx === "01" ? "" : (parseInt(idx) - 1).toString();
+        idxSc = idxSc.length === 1 ? "0".concat(idxSc) : idxSc;
+                                                       
+        let sc = Object.assign({ 'rule': `visible if dbt${idxSc}_ulteriore_derivazione_gestire == 'SI'` }, jsonObject[`derivazioni_bt_${idx}`]);
+
+        let resultSingle = {};
+        resultSingle[`derivazioni_bt_${idx}`] = sc;
+        fs.writeFileSync(config.destination + '/' + `derivazioni_bt_${idx}.json`, JSON.stringify(resultSingle, null, 2));
+
+        resultAll[`derivazioni_bt_${idx}`] = sc;
+
+    });
+
+    fs.writeFileSync(config.destination + '/' + `derivazioni_btAll.json`, JSON.stringify(resultAll, null, 2));
+
+};
+
+const makeQuadroBt = (config) => {
+
+    let resultAll = {};
+    let idxes = ["01", "02", "03"];
+    idxes.forEach(idx => {
+
+        let sourceData = fs.readFileSync(config.source, 'utf8');
+        let outData = sourceData.replace(/"quadro_bt"/g, `"quadro_bt_${idx}"`);
+        outData = outData.replace(/"Quadro BT"/g, `"Quadro BT ${idx}"`);
+        // outData = outData.replace(/rule_/g, `rule${idx}_`);
+        outData = outData.replace(/qbt_/g, `qbt${idx}_`);
+
+        let jsonObject = JSON.parse(outData);
+
+        let idxSc = idx === "01" ? "" : (parseInt(idx) - 1).toString();
+        idxSc = idxSc.length === 1 ? "0".concat(idxSc) : idxSc;
+                                                       
+        let sc = Object.assign({ 'rule': `visible if qbt${idxSc}_ulteriore_derivazione_gestire == 'SI'` }, jsonObject[`quadro_bt_${idx}`]);
+
+        let resultSingle = {};
+        resultSingle[`quadro_bt_${idx}`] = sc;
+        fs.writeFileSync(config.destination + '/' + `quadro_bt_${idx}.json`, JSON.stringify(resultSingle, null, 2));
+
+        resultAll[`quadro_bt_${idx}`] = sc;
+
+    });
+
+    fs.writeFileSync(config.destination + '/' + `quadro_btAll.json`, JSON.stringify(resultAll, null, 2));
+
+};
+
+// makeScomparti({
+//     source: path.join(__dirname, '../../config/json_split/scomparti.json'),
+//     destination: path.join(__dirname, 'out')
+// });
+
+// makeTrasformatoriMtBt({
+//     source: path.join(__dirname, '../../config/json_split/trasformatori_mt_bt.json'),
+//     destination: path.join(__dirname, 'out')
+// });
+
+// makeDerivazioniBt({
+//     source: path.join(__dirname, '../../config/json_split/derivazioni_bt.json'),
+//     destination: path.join(__dirname, 'out')
+// });
+
+makeQuadroBt({
+    source: path.join(__dirname, '../../config/json_split/quadro_bt.json'),
+    destination: path.join(__dirname, 'out')
+});
+
+
+
